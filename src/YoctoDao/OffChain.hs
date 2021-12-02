f29ef89d46217edd2785e503e62453ea8c18a4ff
Riley-Kilgore <rileyk9@icloud.com>
2021-12-02 15:31:45 -0800

    Modularized the code and added a single contract Monad.

--- src/YoctoDao/OffChain.hs
+++ src/YoctoDao/OffChain.hs
@@ -41,6 +41,7 @@ import           PlutusTx.IsData
 import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
 import           Ledger                 hiding (singleton)
 import           Ledger.Ada             as Ada
+import           Ledger.Address         as Address
 import           Ledger.Constraints     as Constraints
 import           Ledger.Index           as Index
 import qualified Ledger.Typed.Scripts   as Scripts
@@ -55,6 +56,10 @@ import           GHC.Generics         (Generic)
 import           Data.String          (IsString (..))
 import           Data.Aeson           (ToJSON, FromJSON)
 import           Playground.Contract
+
+import           YoctoDao.Core
+import           YoctoDao.GovToken
+import           YoctoDao.Treasury
 
 {-- We need to create the proposal using the IdMaker NFT so that it will be valid, also ensure a valid proposal..
 data CreateProposal = CreateProposal
@@ -84,25 +89,54 @@ createProposal cp = do
             , no = 0
             } --}
         
--- We need to create the Ownership Datum UTxO with the IdMaker NFT so that it will be valid for use in proposals.
-data CreateOwnerId = CreateOwnerId
-    { createOwner :: !PubKeyHash
-    } deriving (Show, Generic, FromJSON, ToJSON)
-
-createOwnerId :: forall w s. CreateOwnerId -> Contract w s Text ()
-createOwnerId co = do
-    scriptPubKeyHash <- Contract.ownPubKeyHash
+createOwnerId :: forall w s. ValidatorHash -> AssetClass -> AssetClass -> AssetClass -> AssetClass -> AssetClass -> Integer -> Contract w s Text ()
+createOwnerId treasury govClass nft idMaker propclass voteclass co = do
+    let scriptAddr = votingPassScriptAddress treasury govClass nft idMaker propclass voteclass
+        treasuryAddr = Address.scriptHashAddress treasury
+    pkh <- Contract.ownPubKeyHash
     now <- currentTime
-    -- utxos <- Map.filter idIdMaker <$> utxoAt
-    let ownership = Ownership
-            { owner = createOwner co
+    utxos <- utxosAt scriptAddr
+    futxos <- utxosAt (Address.pubKeyHashAddress pkh)
+
+    let utxos' = Map.filter (\v -> 1 == assetClassValueOf (_ciTxOutValue v) idMaker) utxos
+        futxos' = Map.filter (\v -> 1 <= assetClassValueOf (_ciTxOutValue v) govClass) futxos
+        (nftRef,nftTx) = head $ Map.toList utxos'
+        valueToScript = assetClassValue voteclass 1 <> assetClassValue govClass co
+        ownership = Ownership
+            { owner = pkh
             , nftSlot = now
             , lastTransfer = now
             }
+        oDatum = Datum $ toBuiltinData ownership
+
+        validator = votingPassValidatorScript treasury govClass nft idMaker propclass voteclass
+        validatorHash = votingPassValidatorHash treasury govClass nft idMaker propclass voteclass
+
+        lookups = Constraints.ownPubKeyHash pkh <>
+                  Constraints.mintingPolicy (policy idMaker) <>
+                  Constraints.otherScript validator <>
+                  Constraints.unspentOutputs utxos' <>
+                  Constraints.unspentOutputs futxos'
+
+        tx = Constraints.mustSpendScriptOutput nftRef (Redeemer $ toBuiltinData ()) <>
+             Constraints.mustMintValue (assetClassValue voteclass 1) <>
+             Constraints.mustPayToOtherScript validatorHash oDatum valueToScript
+
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
     Contract.logInfo @String $ printf "Created Ownership: %s" (show ownership)
-  where
-    isIdMaker :: TxOutTx -> Bool
-    isIdMaker o = True
+
+data SpendOwned = SpendOwned
+    { spendAddr  :: !Address
+    , spendVal   :: !Value
+    , spendDatum :: Datum
+    } deriving (Show, Generic, FromJSON, ToJSON)
+
+spendOwnershipUTxO :: forall w s. ValidatorHash -> AssetClass -> AssetClass -> AssetClass -> AssetClass -> AssetClass -> SpendOwned -> Contract w s Text ()
+spendOwnershipUTxO treasury govClass nft idMaker propclass voteclass spend = do
+    Contract.logInfo @String $ printf "Spent owned UTxO to: %s" (show spend)
+
+-- findIdMaker :: forall w s. Address -> AssetClass -> Contract w s Text ()
 
 -- We need to create a transaction that if going to the script will contain the Id for the script,
 --data ShiftOwnership = ShiftOwnership
